2f7f332050f89096d75aa39952a3fe0cdaf2cbda
Ihoeon <102536136+Ihoeon@users.noreply.github.com>
2024-12-16 15:49:08 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20210948/w24Back/W24BackApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20210948.w24Back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24BackApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24BackApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20210948/w24Back/W24BackApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210948.w24Back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24BackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
